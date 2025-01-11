a2a0bb132c97325aeadf22052b17be9e5aa3b473
Wallace-Albuquerque <118636622+WallaceAlbuquerque@users.noreply.github.com>
2025-01-11 13:32:18 -0300

    Update Projeto-dio.py

--- Projeto-dio.py.py
+++ Projeto-dio.py.py
@@ -2,20 +2,20 @@ nome = str(input("Digite o nome do seu héroi: "))
 nivel = eval(input("Digite seu nível: "))
 
 if nivel <= 1000:
-    print("Ferro")
-elif nivel > 1001 and nivel <= 2000:
-    print("Bronze")
-elif nivel > 2001 and nivel <= 5000:
-    print("Prata")
-elif nivel > 5001 and nivel <= 7000:
-    print("Ouro")
-elif nivel > 7001 and nivel <= 8000:
-    print("Platina")
-elif nivel > 8001 and nivel <= 9000:
-    print("Ascendente")
-elif nivel > 9001 and nivel <= 10000:
-    print("Imortal")
+    elemento = "Ferro"
+elif nivel <= 2000:
+    elemento = "Bronze"
+elif nivel <= 5000:
+    elemento = "Prata"
+elif nivel <= 7000:
+    elemento = "Ouro"
+elif nivel <= 8000:
+    elemento = "Platina"
+elif nivel <= 9000:
+    elemento = "Ascendente"
+elif nivel <= 10000:
+    elemento = "Imortal"
 else:
-    print("Radiante")
-    
-print(f"O héroi de nome {nome} está no nível", + nivel)
+    elemento = "Radiante"
+
+print(f"O herói de nome {nome} está no nível {elemento}")
